b4b1cced5c139404f1ed29d711a3fb9ecae5e242
Kwame Davour <kwamedavour@gmail.com>
2016-11-07 14:43:11 +0000

    added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,10 @@
 var socket = io();
+
 socket.on('connect',function (){
 	console.log('connected to socket.io server!');
-})
+});
+
+socket.on('message', function (message){
+	console.log('New message:');
+	console.log(message.text);
+});
